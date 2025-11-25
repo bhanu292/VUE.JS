6a9258b0ce37a01be0be8b389990848a9fe98109
bhanu292 <bharat123tiwari123@gmail.com>
2025-11-25 13:32:26 +0530

    Add files via upload

---
+++ method.vue
@@ -0,0 +1,43 @@
+<template>
+<h1>Email: {{ email }}</h1>
+<h1>Name: {{ getName() }}</h1>
+<h1>Name: {{ getNick("Bhanu") }}</h1>
+<h1>Name 2: {{ getNick("Alisha") }}</h1>
+<h1>All Data:{{ getData() }}</h1>
+<h1>All Data: {{ getData().name }}</h1>
+<h1>All Data: {{ getData().email }}</h1>
+
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            email: 'bharat123tiwari123@gmail.com'
+        }
+    },
+    methods:{
+        getName() {
+            return "Bharat Tiwari"
+        },
+        getNick(name){
+            return name;
+        },
+        getData() {
+            return {
+                name:"Akhil",
+                email:"akhil@gmail.com",
+               // email:this.email
+
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: orange;
+}
+</style>
